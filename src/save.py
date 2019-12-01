ec77213c94fec32687c9cab423a6d118118b67c7
EZE2 <skyeight73@gmail.com>
2019-12-01 22:37:45 +0900

    code concentrate

---
+++ src/save.py
@@ -0,0 +1,42 @@
+import time
+
+'''timer start whem push the key'''
+''' 'c:\users\default\desktop'       default file location?'''
+
+class note_class:
+    def __init__(self,key,interval):
+        self.note_key=key
+        self.note_interval=interval
+        self.time = time.time()
+        '''
+        event_key to key
+        interval is length of the note
+        '''
+
+class sheet_class:
+    def __init__(self):
+        self.note_list = list()
+        self.title="title of the song : "
+        self.instrument=1
+
+    def add_to_sheet(self, note):
+        if len(self.note_list)==0 :
+            self.note_list[0] = note
+        else :
+            self.note_list.append(note)
+
+    def set_title(self):
+        tmp=input("type the title of the song you will play")
+        self.title=tmp
+
+    def set_instrument(self):
+        print("choose the instrument you wanna play'")
+        tmp=input("1.piano'\n' 2.violin '\n' 3.orcarina '\n' 4. voice '\n' : ")
+        if tmp==1 : pass
+        elif tmp==2 : tmp=41
+        elif tmp==3 : tmp=80
+        elif tmp==4 : tmp=54
+        else :
+            print("u chosed wrong instrument")
+
+        self.instrument=tmp
--- src/test.py
+++ src/test.py
@@ -1,6 +1,55 @@
+import keyboard
 import pygame
-import music21
+import time
+import pygame.midi
+import threading
 
-print("hello world!")
+# initialize pygame to use
+pygame.midi.init()
 
-# 작성하면 됨.
+#class KeyInputManager:
+#    def key_input(self, key, note):
+
+
+class Instrument:
+    player = pygame.midi.Output(1)
+
+    def __init__(self, inst_no):
+        self.player.set_instrument(inst_no, 1)
+
+    def set_instrument(self, inst_no):
+        self.player.set_instrument(inst_no, 1)
+
+    def note_on(self, note):
+        self.player.note_on(note, 127, 1)
+        time.sleep(0.1)
+
+    def note_off(self, note):
+        self.player.note_off(note, 127, 1)
+
+# test zone--------------------------------------------------
+
+
+def key_input(key, note):
+    while True:
+        if keyboard.is_pressed(key):
+            print("q pressed!")
+            inst1.note_on(note)
+            # player.note_on(60, 127, 1) # 첫 파라미터가 음 60이 도
+            time.sleep(0.1)
+            while keyboard.is_pressed(key):
+                time.sleep(0.1)
+        else:
+            inst1.note_off(note)
+            # player.note_off(60, 127, 1)
+
+
+inst1 = Instrument(1)
+thread1 = threading.Thread(target=key_input, args=('q', 60))
+thread2 = threading.Thread(target=key_input, args=('w', 62))
+thread3 = threading.Thread(target=key_input, args=('e', 64))
+
+# key_input('q', 60)
+thread1.start()
+thread2.start()
+thread3.start()
