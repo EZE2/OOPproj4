6ecb8964159614caf17031d05078d2220a03f1cc
Sudal <kimkusyeon@gmail.com>
2019-12-08 03:38:09 +0900

    GUI patch(label)
    and I add some explain

--- src/GUI.py
+++ src/GUI.py
@@ -1,17 +1,31 @@
 # -*- coding: utf-8 -*-
 import tkinter as tk
 from tkinter import *
+from tkinter.font import Font
 from PIL import Image, ImageTk
 import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
-IMG_PATH = os.path.join(BASE_PATH,'resource')
+IMG_PATH = os.path.join(BASE_PATH, 'resource')
 
+"""
+# GUI Modules
+# Execution Environment - Python 3.7.4 version
+# Used tkinter, tkinter.font almost / PIL for background, button image
+# There are 6 Classes here,
+# MyFrame, KeyboardGUI(White/BlackPianoButton(child class)),
+# RecordGUI, InstrumentsGUI
+"""
+
+root = Tk()
 WIDTH  = 600  # Background image width
 HEIGHT = 338  # Background image height
-root = Tk()
 
-
+"""
+" For Window / Background
+" using tk Canvas for loading background and call image with ImageTk(PIL) 
+" set Frame to secure space for background
+"""
 class MyFrame:
     def __init__(self):
         root.geometry('600x538')
@@ -19,12 +33,12 @@ class MyFrame:
         root.resizable(False, False)
 
         self.canvas = Canvas(root, width=WIDTH, height=HEIGHT)
-        self.bg_img = ImageTk.PhotoImage(Image.open(os.path.join(IMG_PATH,'gris.jpg')).resize((WIDTH, HEIGHT), Image.ANTIALIAS))
+        self.bg_img = ImageTk.PhotoImage(Image.open(os.path.join(IMG_PATH, 'gris.png')).resize((WIDTH, HEIGHT), Image.ANTIALIAS))
         self.canvas.background = self.bg_img
         self.bg = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.bg_img)
         self.canvas.place(x=0, y=0)
 
-        self.right_frame = Frame(root, width=1, height=340, bg='black')
+        self.right_frame = Frame(root, width=1, height=HEIGHT, bg='black')
         self.right_frame.grid(row=0, column=30)
         self.right_frame['borderwidth'] = 0
 
@@ -33,11 +47,18 @@ class MyFrame:
         # self.left_frame['borderwidth'] = 0
 
 
-# Piano Button list(Each of these is a Button name)
+# Piano Button list(Each of these is a Button name and respond to same keyboard input)
 white_button_list = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';']
 black_button_list = ['w', 'e', 'NULL', 't', 'y', 'u', 'NULL', 'o', 'p']
 
 
+"""
+# Keyboard GUI
+# WhitePianoButton and BlackPianoButton are child class of KeyboardGUI class
+# keyboardGUI is used in main.py's key_input function
+# made with tk Button, grid
+# buttons have two state - Pressed or Unpressed
+"""
 class WhitePianoButton(Button):
     def makename(self, name):
         self.name = name
@@ -67,7 +88,7 @@ class BlackPianoButton(Button):
 
 
 class KeyboardGUI:
-    button_list = list()
+    button_list = list()  # It has white button list first and append black "
 
     def __init__(self):
         scales = 1
@@ -94,17 +115,22 @@ class KeyboardGUI:
             root.rowconfigure(i+1, weight=1)
 
 
+"""
+# Record Button GUI
+# made with tk Button and Using PhotoImage(PIL)
+# button also has two state - recording / stopped
+"""
 class RecordGUI:
     def __init__(self):
         self.record_img = PhotoImage(file=os.path.join(IMG_PATH, 'recordbutton.png'))
         self.stop_img = PhotoImage(file=os.path.join(IMG_PATH, 'stopbutton.png'))
 
-        self.button = Button(root)
+        self.button = Button(root, command=self.recording)
         self.button.config(image=self.record_img, width=95, height=30, bd=0)
         self.button.place(x=500, y=25)
 
-        self.button.bind('<Button-1>', self.recording())
-        self.button.bind('<Return>', self.stop_recording())
+        # self.button.bind('<Button-1>', self.recording())
+        # self.button.bind('<Return>', self.stop_recording())
 
         # self.button = PlayButton(root, width=7, height=3, text="PLAY", bg='black', fg='white')
         # self.button.grid(row=0, column=30)
@@ -113,18 +139,40 @@ class RecordGUI:
 
     def recording(self):
         self.button.config(image=self.stop_img)
-        # start recording
         self.button['command'] = self.stop_recording
+        # start recording
 
     def stop_recording(self):
         self.button.config(image=self.record_img)
         self.button['command'] = self.recording
 
+
+# Used to show what instrument is currently playing with tk Label and Font
+class PlayLabelGUI:
+    def __init__(self):
+        self.label_font = tk.font.Font(root, family='Arial', size=17, weight='bold')
+        self.label = Label(root, width=10, height=1, bg='black', bd=0,
+                           text="Piano", fg='white', anchor='w')
+        self.label['font'] = self.label_font
+        self.label.place(x=500, y=92)
+        # self.label_button = Button(root, bg='black', bd=0, command=self.change)
+        # self.label_button.place(x=600, y=10)
+
+    def change(self, _key):
+        if _key == 1:
+            self.label.configure(text="Piano")
+        elif _key == 2:
+            self.label.configure(text="Acoustic Guitar")
+        elif _key == 3:
+            self.label.configure(text="Violin")
+
+
+# init all in GUI
 def GUIinit():
     myframe = MyFrame()
     keyboardgui = KeyboardGUI()
     recordgui = RecordGUI()
-
+    playlabelgui = PlayLabelGUI()
 
 
 
--- src/main.py
+++ src/main.py
@@ -19,8 +19,8 @@ import keyboard
 import time
 import pygame.midi
 import threading
-from live_play import Instrument, key_list2, key_list, note_list, note_list2, midi_dic # live_play 구현부분 모듈화
-from GUI import GUIinit, KeyboardGUI, root
+from src.live_play import Instrument, key_list2, key_list, note_list, note_list2, midi_dic # live_play 구현부분 모듈화
+from src.GUI import GUIinit, KeyboardGUI, PlayLabelGUI, root
 
 # initialize pygame to use
 pygame.midi.init()
@@ -40,6 +40,7 @@ def option(instrument):
     while True:
         if keyboard.is_pressed('1'):
             inst_key_pressed(instrument, 'piano')
+            # PlayLabelGUI.label.configure(text="Piano")
         elif keyboard.is_pressed('2'):
             inst_key_pressed(instrument, 'acoustic guitar')
         elif keyboard.is_pressed('3'):
