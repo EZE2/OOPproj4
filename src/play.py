71459330605d7e56888f64f6473fbe653be75138
jktg94 <rnwns0815@naver.com>
2019-12-07 19:10:02 +0900

    Create play.py

---
+++ src/play.py
@@ -0,0 +1,33 @@
+
+from tkinter import *
+from tkinter import filedialog
+import pygame.midi
+import threading
+import time
+
+
+pygame.midi.init()
+
+def make_sheet():
+    root = Tk()
+    root.filename = filedialog.askopenfilename(title="choose txt file",
+                                               filetypes=(("text files", "*.txt"), ("all files", "*.*")))
+
+    f = open(root.filename, 'r')
+    sheet_list = []
+    for item in f.readlines():
+        point = item.split(' ')
+        x = point[0]
+        y = point[1]
+        z = point[2]
+        point_as_array = [x, y, z]
+        sheet_list.append(point_as_array)
+    #print(sheet_list)
+    return sheet_list
+
+
+
+
+
+if __name__ == "__main__":
+    play = music_play()
