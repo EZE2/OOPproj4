9ba657d5cfd68bfc55c45e2a1f53822cfc8c10f6
EZE2 <skyeight73@gmail.com>
2019-12-08 22:37:24 +0900

    completed

--- src/main.py
+++ src/main.py
@@ -70,7 +70,7 @@ def key_input(_key, _note, instrument):
                 continue
             else:
                 instrument.note_on(_note)
-                tmp_note.set_key(_note)
+                tmp_note.set_key(_key)
             for button in KeyboardGUI.button_list:
                 if button.name == _key:
                     button.update()
--- src/play.py
+++ src/play.py
@@ -11,34 +11,6 @@ pygame.midi.init()
 
 _key = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'w', 'e', 't', 'y', 'u', 'o', 'p', '.', '.', '.']
 
-# def make_sheet():
-#
-#     root.filename = filedialog.askopenfilename(title="choose txt file",
-#                                                filetypes=(("text files", "*.txt"), ("all files", "*.*")))
-#
-#     f = open(root.filename, 'r')
-#     sheet_list = []
-#     for item in f.readlines():
-#         item = item.rstrip()
-#         point = item.split(' ')
-#         x = point[0]
-#         y = point[1]
-#         z = point[2]
-#         point_as_array = [x, y, z]
-#         sheet_list.append(point_as_array)
-#     #print(sheet_list)
-#     sheet_list.sort()
-#     return sheet_list
-#
-
-
-# def rec_play():
-#     #player = pygame.midi.Output(0)
-#     a = make_sheet()
-#     print(a)
-#     note_list = filter(lambda x: x[0] == 'a', a)
-#     print(note_list)
-#
 
 '''for i in range(len(note_list)):
         time.sleep(float(note_list[i][2]))
