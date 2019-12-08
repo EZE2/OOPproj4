39f5f0b7fa943fb9ae5a4c1cc1acbf2660dfa3b3
jktg94 <rnwns0815@naver.com>
2019-12-08 21:32:19 +0900

    Update play.py

--- src/play.py
+++ src/play.py
@@ -26,13 +26,12 @@ def make_sheet():
         z = point[2]
         point_as_array = [x, y, z]
         sheet_list.append(point_as_array)
-    #print(sheet_list)
+    print(sheet_list)
     sheet_list.sort()
     return sheet_list
 
 
 def rec_play(key):
-    a = make_sheet()
     note_list = list(filter(lambda x: x[0] == key, a))
     for i in range(len(note_list)):
         time.sleep(float(note_list[i][2]))
@@ -42,26 +41,22 @@ def rec_play(key):
 
 
 def p_thread_initializer(play_key):
-    p_thread1 = threading.Thread(target=rec_play, args=(play_key[0]), daemon=True)
-    p_thread2 = threading.Thread(target=rec_play, args=(play_key[1]), daemon=True)
-    p_thread3 = threading.Thread(target=rec_play, args=(play_key[2]), daemon=True)
-    p_thread4 = threading.Thread(target=rec_play, args=(play_key[3]), daemon=True)
-    p_thread5 = threading.Thread(target=rec_play, args=(play_key[4]), daemon=True)
-    p_thread6 = threading.Thread(target=rec_play, args=(play_key[5]), daemon=True)
-    p_thread7 = threading.Thread(target=rec_play, args=(play_key[6]), daemon=True)
-    p_thread8 = threading.Thread(target=rec_play, args=(play_key[7]), daemon=True)
-    p_thread9 = threading.Thread(target=rec_play, args=(play_key[8]), daemon=True)
-    p_thread10 = threading.Thread(target=rec_play, args=(play_key[9]), daemon=True)
-    p_thread11 = threading.Thread(target=rec_play, args=(play_key[10]), daemon=True)
-    p_thread12 = threading.Thread(target=rec_play, args=(play_key[11]), daemon=True)
-    p_thread13 = threading.Thread(target=rec_play, args=(play_key[12]), daemon=True)
-    p_thread14 = threading.Thread(target=rec_play, args=(play_key[13]), daemon=True)
-    p_thread15 = threading.Thread(target=rec_play, args=(play_key[14]), daemon=True)
-    p_thread16 = threading.Thread(target=rec_play, args=(play_key[15]), daemon=True)
-    p_thread17 = threading.Thread(target=rec_play, args=(play_key[16]), daemon=True)
-    p_thread18 = threading.Thread(target=rec_play, args=(play_key[17]), daemon=True)
-    p_thread19 = threading.Thread(target=rec_play, args=(play_key[18]), daemon=True)
-    p_thread20 = threading.Thread(target=rec_play, args=(play_key[19]), daemon=True)
+    p_thread1 = threading.Thread(target=rec_play, args=(play_key[0],), daemon=True)
+    p_thread2 = threading.Thread(target=rec_play, args=(play_key[1],), daemon=True)
+    p_thread3 = threading.Thread(target=rec_play, args=(play_key[2],), daemon=True)
+    p_thread4 = threading.Thread(target=rec_play, args=(play_key[3],), daemon=True)
+    p_thread5 = threading.Thread(target=rec_play, args=(play_key[4],), daemon=True)
+    p_thread6 = threading.Thread(target=rec_play, args=(play_key[5],), daemon=True)
+    p_thread7 = threading.Thread(target=rec_play, args=(play_key[6],), daemon=True)
+    p_thread8 = threading.Thread(target=rec_play, args=(play_key[7],), daemon=True)
+    p_thread9 = threading.Thread(target=rec_play, args=(play_key[8],), daemon=True)
+    p_thread10 = threading.Thread(target=rec_play, args=(play_key[9],), daemon=True)
+    p_thread11 = threading.Thread(target=rec_play, args=(play_key[10],), daemon=True)
+    p_thread12 = threading.Thread(target=rec_play, args=(play_key[11],), daemon=True)
+    p_thread13 = threading.Thread(target=rec_play, args=(play_key[12],), daemon=True)
+    p_thread14 = threading.Thread(target=rec_play, args=(play_key[13],), daemon=True)
+    p_thread15 = threading.Thread(target=rec_play, args=(play_key[14],), daemon=True)
+    p_thread16 = threading.Thread(target=rec_play, args=(play_key[15],), daemon=True)
 
     p_thread1.start()
     p_thread2.start()
@@ -79,13 +74,10 @@ def p_thread_initializer(play_key):
     p_thread14.start()
     p_thread15.start()
     p_thread16.start()
-    p_thread17.start()
-    p_thread18.start()
-    p_thread19.start()
-    p_thread20.start()
+
 
 
 if __name__ == "__main__":
-
+    a = make_sheet()
     p_thread_initializer(play_key)
 
